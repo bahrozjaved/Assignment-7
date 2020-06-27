b7e8eb6844d67881b05800cabb53fecf7697adaa
bahrozjaved <bahrozjaved6@gmail.com>
2020-06-27 14:18:45 +0500

    new files added

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcNumbers(result) {
+    form.Result1.value = form.Result1.value + result;
+}
+
+function clearResult() {
+    var result = document.getElementById("result");
+    result.value = "";
+}
